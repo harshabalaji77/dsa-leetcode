c6ae5a292c4c70e27c973e8f7621ea0d31430140
Harsha <harshabalaji77@gmail.com>
2026-06-01 23:44:33 +0530

    First Unique Even Element

---
+++ Arrays/First-Unique-Even-Element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int firstUniqueEven(int[] nums) {
+        int[] freq = new int[101];
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        for (int num : nums) {
+            if (num % 2 == 0 && freq[num] == 1) {
+                return num;
+            }
+        }
+        return -1;
+    }
+}
